eb6fd855f0141cbdece416e065c639aa72526d88
philouLeF <delcroixphilippe73@gmail.com>
2022-07-27 16:25:14 +0200

    ajout du style

--- react-ts-shopping/src/Item/Item.tsx
+++ react-ts-shopping/src/Item/Item.tsx
@@ -3,7 +3,7 @@ import { Button } from "@material-ui/core";
 // Types
 import { CartItemType } from "../App";
 // Styles
-import { Wrapper } from "../App.styles";
+import { Wrapper } from "./Item.styles";
 
 type Props = {
     item: CartItemType,
